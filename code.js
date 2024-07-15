04925e1bd95a46d75ead61195aa1db82a76bda33
Sidharth <sidharthinfernal@gmail.com>
2024-07-15 14:19:41 +0530

    initailize and board setup

---
+++ code.js
@@ -0,0 +1,46 @@
+let rows = 30;
+let cols = 30;
+
+let grid = new Array(30);
+
+
+
+function createTable(){
+    let gridContainer = document.getElementById('gridContainer');
+    let table = document.createElement('table');
+    for(let i = 0; i < rows; i++){
+        let tr = document.createElement('tr');
+        for(let j = 0; j < cols; j++){
+            let cell = document.createElement('td');
+            cell.setAttribute("id", i + "_" + j);
+            cell.setAttribute('class',"dead");
+            cell.onclick = cellClickHandler;
+            
+            tr.appendChild(cell);
+        }
+        table.appendChild(tr)
+    }
+    gridContainer.appendChild(table);
+}
+
+createTable();
+
+
+
+function cellClickHandler() {
+    var rowcol = this.id.split("_");
+    var row = rowcol[0];
+    var col = rowcol[1];
+    
+    var classes = this.getAttribute("class");
+    if(classes.indexOf("live") > -1) {
+        this.setAttribute("class", "dead");
+        grid[row][col] = 0;
+    } else {
+        this.setAttribute("class", "live");
+        grid[row][col] = 1;
+    }
+    
+}
+
+
