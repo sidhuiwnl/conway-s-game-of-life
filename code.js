c5bb5fe329293c2d7fc9acd4e2b415ad902936de
Sidharth <sidharthinfernal@gmail.com>
2024-07-15 19:32:19 +0530

    done

--- code.js
+++ code.js
@@ -3,8 +3,9 @@ let cols = 30;
 
 let playing = false;
 let grid = new Array(rows);
-
+let timer;
 let nextGrid = new Array(rows);
+let reproductionTime = 100;
 
 
 
@@ -26,8 +27,7 @@ function createTable(){
     gridContainer.appendChild(table);
 }
 
-createTable();
-updateView();
+
 
 
 function cellClickHandler() {
@@ -60,18 +60,18 @@ function updateView(){
     }
 }
 
-function applyingRules(col,row){
+function applyingRules(row,col){
     let neighbourCount = countLiveNeighbors(col,row);
     if(grid[row][col] === 1){
         if(neighbourCount < 2){
             nextGrid[row][col] = 0;
-        }else if(neighbourCount === 2 && neighbourCount === 3){
+        }else if(neighbourCount === 2 || neighbourCount === 3){
             nextGrid[row][col] = 1;
         }else if(neighbourCount > 3){
             nextGrid[row][col] = 0;
         }
     }else if (grid[row][col] == 0) {
-        if (numNeighbors == 3) {
+        if (neighbourCount == 3) {
             nextGrid[row][col] = 1;
         }
     }
@@ -80,11 +80,11 @@ function applyingRules(col,row){
 
 function countLiveNeighbors(col,row){
     let neighbor = 0;
-    for(let i = -1; i < 1; i++){
-        for(let j = -1; j < 1; j++){
+    for(let i = -1; i <= 1; i++){
+        for(let j = -1; j <= 1; j++){
             if(i === 0 && j===0) continue
-            let newRow = row+1;
-            let newCol = col+1;
+            let newRow = row + i;
+            let newCol = col + j;
             if(newRow >=0 && newRow < rows && newCol >= 0 && newCol < cols){
                 neighbor += grid[newRow][newCol];
             }
@@ -95,6 +95,129 @@ function countLiveNeighbors(col,row){
 }
 
 
+function setUpControlButtons(){
+    let startButton = document.getElementById('start');
+    startButton.onclick = startButtonHandler;
+
+    let clearButton = document.getElementById('clear');
+    clearButton.onclick = clearButtonHandler;
+    
+    // button to set random initial state
+    let randomButton = document.getElementById("random");
+    randomButton.onclick = randomButtonHandler;
+}
+
+
+function startButtonHandler() {
+    if (playing) {
+        console.log("Pause the game");
+        playing = false;
+        this.innerHTML = "Continue";
+        clearTimeout(timer); // Stop the periodic updates
+    } else {
+        console.log("Continue the game");
+        playing = true;
+        this.innerHTML = "Pause";
+        play();
+    }
+}
+
+
+function clearButtonHandler(){
+    let startButton = document.getElementById('start');
+
+    playing = false;
+
+    startButton.innerHTML = "Start";
+    clearTimeout(timer);
+
+    let liveCells = document.getElementsByClassName('live');
+    let cells = []
+
+    for(let i = 0; i < liveCells.length; i++){
+        cells.push(liveCells[i]);
+
+    }
+
+    for(let i = 0; i < cells.length; i++){
+        cells[i].setAttribute('class','dead')
+    }
+
+    resetGrids();
+
+
+}
+
+
+function randomButtonHandler() {
+    if (playing) return;
+    clearButtonHandler();
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            var isLive = Math.round(Math.random());
+            if (isLive == 1) {
+                var cell = document.getElementById(i + "_" + j);
+                cell.setAttribute("class", "live");
+                grid[i][j] = 1;
+            }
+        }
+    }
+}
+
+function initializeGrids() {
+    for (var i = 0; i < rows; i++) {
+        grid[i] = new Array(cols);
+        nextGrid[i] = new Array(cols);
+    }
+}
+
+function resetGrids() {
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            grid[i][j] = 0;
+            nextGrid[i][j] = 0;
+        }
+    }
+}
+
+function copyAndResetGrid() {
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            grid[i][j] = nextGrid[i][j];
+            nextGrid[i][j] = 0;
+        }
+    }
+}
+
+function play() {
+    computeNextGen();
+    
+    if (playing) {
+        clearTimeout(timer)
+        timer = setTimeout(play, reproductionTime);
+    }
+}
+
+function computeNextGen() {
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            applyingRules(i, j);
+        }
+    }
+    
+    
+    copyAndResetGrid();
+    
+    updateView();
+}
 
 
 
+function initialize() {
+    createTable();
+    initializeGrids();
+    resetGrids();
+    setUpControlButtons();
+}
+
+window.onload = initialize;
